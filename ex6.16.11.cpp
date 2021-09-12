67a5b66062c75d8a732249ab76a36109e66d8030
cardigan1008 <87902024+cardigan1008@users.noreply.github.com>
2021-09-13 00:33:24 +0800

    modify1
    some changes

--- ex6.16.11.cpp
+++ ex6.16.11.cpp
@@ -1,19 +1,20 @@
 #include<stdio.h>
 int main(void)
 {
-	int num1,num2,num,sum;
+	int num1,num2,num,sum,status;
 	sum = 0;
 	
 	printf("Enter 2 numbers\n");
-	scanf("%d %d",&num1,&num2);
+	status =scanf("%d %d",&num1,&num2);
 	
-	while(num1 < num2)
+	while(num1 < num2 && status == 2)
 	{
 		for(num = num1;num <= num2;num ++)
 			sum = sum + num*num;
 	
 		printf("sum = %d\n",sum);
 		
+		printf("Enter 2 numbers\n");
 		scanf("%d %d",&num1,&num2);
 	}
 	
